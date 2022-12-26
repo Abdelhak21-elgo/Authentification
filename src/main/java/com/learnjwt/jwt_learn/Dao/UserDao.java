48374e400e5f4065ab8189f02935a81b416668f2
abdelhak elgourmat <andelhakabdelhak200@gmail.com>
2022-12-26 13:27:57 +0100

    add getallproduct api

--- src/main/java/com/learnjwt/jwt_learn/Dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.learnjwt.jwt_learn.Dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.learnjwt.jwt_learn.Entity.User;
-
-public interface UserDao extends CrudRepository<User,String>{
-    
-}
